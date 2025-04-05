8376caa058d002b7331ebd06eaaa657d180814f4
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2025-04-05 08:59:27 +0530

    write bunch of queries for the doc handlers

---
+++ sql/queries/docs.sql
@@ -0,0 +1,27 @@
+
+-- name: CreateDoc :one
+INSERT INTO docs(
+    id,
+    created_at,
+    updated_at,
+    user_id
+)VALUES(
+    gen_random_uuid(),
+    NOW(),
+    NOW(),
+    $1
+)
+RETURNING *;
+
+-- name: GetDocByID :one
+SELECT * FROM docs
+WHERE id=$1;
+
+-- name: UpdateContent :exec
+UPDATE docs
+SET content=$1, updated_at=NOW()
+WHERE id=$2;
+
+-- name: DeleteDocByID :exec
+DELETE FROM docs
+WHERE id=$1;
